3fb13af365aa1c204752c4c7f773341c73c0126e
Ilya Stroev <ilya_stroev@mail.ru>
2023-06-07 23:29:58 +0300

    step9

--- src/main/java/com/example/collection/EmployeeService.java
+++ src/main/java/com/example/collection/EmployeeService.java
@@ -13,37 +13,50 @@ public class EmployeeService {
     private final Integer maxEmployee = 100;
 
 
-    public void addEmployee(Employee employee){
-        if (employees.size()<maxEmployee){
-            if(employeeIsAdded(employee)){
+    public void addEmployee(Employee employee) {
+        if (employees.size() < maxEmployee) {
+            if (employeeIsAdded(employee)) {
                 throw new EmployeeAlreadyAddedException();
-            }else {
+            } else {
                 employees.add(employee);
             }
         } else {
             throw new EmployeeStorageIsFullException();
         }
     }
-    public void deleteEmployee(Integer index){
-        employees.remove(index);
+
+    public void deleteEmployee(Integer index) {
+        if (index > employees.size()) {
+            throw new EmployeeNotFoundException();
+        } else {
+            employees.remove(index);
+        }
+    }
+    public void deleteEmployee(Employee employee) {
+        if (employeeIsAdded(employee)) {
+            employees.remove(employee);
+        } else {
+            throw new EmployeeNotFoundException();
+        }
     }
 
-    public Employee searchEmployee(String nameOrSurname){
+
+    public Employee searchEmployee(String nameOrSurname) {
         Employee employee = null;
         for (int i = 0; i < employees.size(); i++) {
             employee = employees.get(i);
             if (employee.getName().equals(nameOrSurname) ||
-                    employee.getSurname().equals(nameOrSurname)){
+                    employee.getSurname().equals(nameOrSurname)) {
                 break;
             }
         }
-        if (employee == null){
+        if (employee == null) {
             throw new EmployeeNotFoundException();
         }
         return employee;
     }
 
-    public boolean employeeIsAdded(Employee employee){
+    public boolean employeeIsAdded(Employee employee) {
         for (Employee value : employees) {
             if (value.equals(employee)) {
                 return true;
@@ -51,8 +64,6 @@ public class EmployeeService {
         }
         return false;
     }
-
-
 
 
 }
