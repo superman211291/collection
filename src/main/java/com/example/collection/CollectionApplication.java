7aa4020af7c756813c2124629d57f33b105baa85
Ilya Stroev <ilya_stroev@mail.ru>
2023-06-07 22:12:47 +0300

    create

--- src/main/java/com/example/collection/CollectionApplication.java
+++ src/main/java/com/example/collection/CollectionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CollectionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CollectionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/collection/CollectionApplicationTests.java
+++ src/test/java/com/example/collection/CollectionApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CollectionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
