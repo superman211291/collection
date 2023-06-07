8829ce2e97184ea513bce5358cd2ce0c3b521acf
Ilya Stroev <ilya_stroev@mail.ru>
2023-06-07 23:11:49 +0300

    step6

---
+++ src/main/java/com/example/collection/EmployeeNotFoundException.java
@@ -0,0 +1,4 @@
+package com.example.collection;
+
+public class EmployeeNotFoundException extends RuntimeException{
+}
--- src/main/java/com/example/collection/EmployeeService.java
+++ src/main/java/com/example/collection/EmployeeService.java
@@ -31,6 +31,9 @@ public class EmployeeService {
                break;
             }
         }
+        if (employee == null){
+            throw new EmployeeNotFoundException();
+        }
         return employee;
     }
 
